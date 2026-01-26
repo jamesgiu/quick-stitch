4098d99404947a400f14b85f321f78a4d5b2ee79
James Giuffrida <james.giuffrida@gmail.com>
2026-01-26 23:55:39 +1100

    QS-XXX Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,52 @@
+import "@elastic/eui/dist/eui_theme_dark.css";
+import "./index.scss";
+import "./index.css";
+import "./App.css";
+
+import {
+  EuiLoadingLogo,
+  EuiProvider,
+  EuiEmptyPrompt,
+  EuiLink,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiCard,
+  EuiTitle,
+  EuiIcon,
+} from "@elastic/eui";
+import {Dotting} from "dotting";
+import PixelCanvas from "./PixelCanvas";
+
+const MyApp = () => {
+  return (
+    <EuiProvider>
+      <div className="app-content">
+        <div className={`bg-image-wrapper`} />
+        <EuiEmptyPrompt
+          icon={<div className="app-logo" />}
+          title={
+            <EuiTitle className="logo-subtext" size="l">
+              <h1>QuickStitch</h1>
+            </EuiTitle>
+          }
+        />
+        <EuiFlexGroup className={"eui-flex-dotting"}>
+            <PixelCanvas/>
+        </EuiFlexGroup>
+        <div className="qs-footer">
+          <span>
+            © 2024 {Math.random() > 0.5 ? "Jiv" : "Lames"}{" "}
+            <EuiLink
+              href="https://github.com/jamesgiu/quick-stitch"
+              target="https://github.com/jamesgiu/quick-stitch"
+            >
+              <EuiIcon size="l" type={"logoGithub"} />
+            </EuiLink>
+          </span>
+        </div>
+      </div>
+    </EuiProvider>
+  );
+};
+
+export default MyApp;
---
+++ frontend/src/PixelCanvas.tsx
@@ -0,0 +1,100 @@
+import React, { useRef } from "react";
+import {BrushTool, Dotting, DottingRef, useBrush, useDotting} from "dotting";
+import "./PixelCanvas.css"
+
+const PixelCanvas = () => {
+    const ref = useRef<DottingRef>(null);
+    const { undo, redo } = useDotting(ref);
+    const { changeBrushColor, brushTool, changeBrushTool } = useBrush(ref);
+    return (
+        <div
+            style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                position: "relative",
+            }}
+            className={"pixel-wrapper"}
+        >
+            <Dotting ref={ref} width={"100%"} height={"40vh"} backgroundColor={"rgba(58, 29, 53, 0.30)"} />
+            <div>
+                {[
+                    "#FF0000",
+                    "#0000FF",
+                    "#00FF00",
+                    "#FF00FF",
+                    "#00FFFF",
+                    "#FFFF00",
+                    "#000000",
+                    "#FFFFFF",
+                ].map(color => (
+                    <div
+                        key={color}
+                        onClick={changeBrushColor.bind(null, color)}
+                        style={{
+                            width: 25,
+                            height: 25,
+                            margin: 10,
+                            border: "1px solid black",
+                            backgroundColor: color,
+                            display: "inline-block",
+                        }}
+                    />
+                ))}
+            </div>
+            <div>
+                <select
+                    style={{
+                        marginLeft: 15,
+                    }}
+                    value={brushTool}
+                    onChange={e => {
+                        changeBrushTool(e.target.value as BrushTool);
+                    }}
+                >
+                    <option value={BrushTool.NONE}>{BrushTool.NONE}</option>
+                    <option value={BrushTool.DOT}>{BrushTool.DOT}</option>
+                    <option value={BrushTool.ERASER}>{BrushTool.ERASER}</option>
+                    <option value={BrushTool.PAINT_BUCKET}>
+                        {BrushTool.PAINT_BUCKET}
+                    </option>
+                    <option value={BrushTool.SELECT}>{BrushTool.SELECT}</option>
+                    <option value={BrushTool.LINE}>{BrushTool.LINE}</option>
+                    <option value={BrushTool.RECTANGLE}>{BrushTool.RECTANGLE}</option>
+                    <option value={BrushTool.RECTANGLE_FILLED}>{BrushTool.RECTANGLE_FILLED}</option>
+                    <option value={BrushTool.ELLIPSE}>{BrushTool.ELLIPSE}</option>
+                    <option value={BrushTool.ELLIPSE_FILLED}>{BrushTool.ELLIPSE_FILLED}</option>
+                </select>
+            </div>
+            <div
+                style={{
+                    marginTop: 10,
+                    marginBottom: 50,
+                    display: "flex",
+                }}
+            >
+                <button
+                    style={{
+                        padding: "5px 10px",
+                        background: "none",
+                    }}
+                    onClick={undo}
+                >
+                    undo
+                </button>
+                <button
+                    style={{
+                        padding: "5px 10px",
+                        background: "none",
+                    }}
+                    onClick={redo}
+                >
+                    redo
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default PixelCanvas;
